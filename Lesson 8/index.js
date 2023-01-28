a568349aac93cee5c959ffd7980214a860af021e
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-01-28 09:41:56 +0530

    Made CSS nicer 😋

--- Lesson 8/index.js
+++ Lesson 8/index.js
@@ -4,26 +4,37 @@ import {contractAddress,abi} from "./constant.js"
 const connectButton = document.getElementById("connectButton");
 const fundmeButton = document.getElementById("fund");
 const balanceButton = document.getElementById("balance");
+const flipButton = document.getElementById("flip")
+const flipbackButton = document.getElementById("flipback");
 
+flipButton.onclick = flip;
 connectButton.onclick = connect;
 fundmeButton.onclick = fund;
 balanceButton.onclick = balance;
+flipbackButton.onclick = flip;
+
+function flip(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
 
 async function balance (){
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const balance = await provider.getBalance(contractAddress);
     console.log(ethers.utils.formatEther(balance));
 }
-
+var temp ;
 async function connect(){
     if(typeof window.ethereum != "undefined"){
         console.log("Hello");
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const accnt = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        temp = accnt[0];
         document.getElementById("connectButton").innerText="Connected!"
         document.getElementById("connectButton").remove();
         document.getElementById("metamaskConnect").innerHTML="<div id ='metamaskConnect1'><h2 id ='Connect-Meta'>Connected to <img src='./Images/metamask.png' width='30' height='24'></h2> </div>";
-
-
+        document.getElementById("address").innerHTML=`Connected to account : ${accnt[0]}`;
     }
     else{
         document.getElementById("connectButton").innerText="No metamask! Install it first!"
@@ -38,16 +49,21 @@ async function fund (){
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const contract = new ethers.Contract(contractAddress,abi,signer);
+        
         try{
             const transactionResponse = await contract.fund({value: ethers.utils.parseEther(ethAmount)});
             // console.log(transactionResponse.log);
             await listenForTransactionMine(transactionResponse,provider);
+            
             console.log("Done");
+            flip();
+            document.getElementById('success').innerText = `Funded ${contractAddress} with ${ethAmount} from ${temp}` ;
         }
         
         catch (e){
             console.log(e);
         }
+        document.getElementById("ETHAmount").value="";
     }
 }
 
