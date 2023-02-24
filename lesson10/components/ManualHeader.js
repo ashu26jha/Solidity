2787560aa3f59afb79ae6331ece21537407af36c
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-02-24 18:38:52 +0530

    Header

--- lesson10/components/ManualHeader.js
+++ lesson10/components/ManualHeader.js
@@ -5,18 +5,14 @@ export default function ManualHeader () {
     
     const { enableWeb3, isWeb3Enabled, isWeb3EnableLoading, account, Moralis, deactivateWeb3 } = useMoralis();
 
-    useEffect(()=>{
-        if(isWeb3Enabled){
-            return;
-        }
-        if(typeof window !== "undefined" && window.localStorage.getItem("connected") ){
-            console.log("Nhi tha connect.. Connect kiya")
-            enableWeb3();
-        }
-    },[isWeb3Enabled])
-
     useEffect( ()=> {
-        console.log("Hello")
+        // console.log("Hello");
+        console.log("Account is : ", account);
+        // if(account == null && ){
+        //     window.localStorage.removeItem("connected");
+        //     deactivateWeb3();
+        //     console.log("Null account")
+        // }
         Moralis.onAccountChanged((newAccount)=>{
             console.log(`Account changed to ${console.log(newAccount)}`);
             if(newAccount == null){
@@ -28,7 +24,18 @@ export default function ManualHeader () {
 
     },[])
 
-    
+    useEffect(()=>{
+        if(isWeb3Enabled){
+            return;
+        }
+        if(account == null && window.localStorage.getItem("connected")){
+            enableWeb3();
+        }
+        if(typeof window !== "undefined" && window.localStorage.getItem("connected") ){
+            console.log("Nhi tha connect.. Connect kiya")
+            enableWeb3();
+        }
+    },[isWeb3Enabled])
 
     return(
         <div>
