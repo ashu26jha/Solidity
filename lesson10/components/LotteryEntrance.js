facc2ae19403d6c61924db6fe0901d8f0c6bd432
Ashutosh Jha <ashutosh26jha@gmail.com>
2023-02-25 00:24:52 +0530

    Added enterRaffle()

--- lesson10/components/LotteryEntrance.js
+++ lesson10/components/LotteryEntrance.js
@@ -1,13 +1,65 @@
 // import { func } from 'prop-types';
 import { Contract } from 'ethers';
 import {useWeb3Contract} from 'react-moralis';
+import {frontend,abi} from '../constants/index';
+import { useMoralis } from 'react-moralis';
+import { useEffect, useState } from 'react';
+import {ethers} from 'ethers'
 
 export default function LotteryEntrance() {
-    const {runContractFunction: enterRaffle}= useWeb3Contract(
-        // abi:12,
-        // contractAddress:,
-        // functionName:,
-        // msgValue:,
-        // params: {},
+    const {chainId: chainIdHex, isWeb3Enabled} = useMoralis(); 
+    const chainID = (parseInt(chainIdHex));
+    const contractAddress = chainID in frontend ? frontend[chainID][0] : null;
+    console.log(contractAddress);
+    const [entranceFee,setEntranceFee] = useState("0")
+    let EntranceFeeETH = "";
+
+    const {runContractFunction: enterRaffle}= useWeb3Contract({
+            abi: abi,
+            contractAddress: contractAddress,
+            functionName: "enterRaffle",
+            msgValue: entranceFee,
+            params: {},
+        }
+    )
+
+    const {runContractFunction: getEntranceFee}= useWeb3Contract({
+            abi: abi,
+            contractAddress: contractAddress,
+            functionName: "getEntranceFee",
+            // msgValue:,
+            // params: {},
+        }
+    )
+
+    useEffect(()=>{
+        console.log("H")
+        if(isWeb3Enabled){
+            async function help(){
+                if(contractAddress != null){
+                    const entranceFeeFromContract = (await getEntranceFee()).toString();
+                    setEntranceFee(entranceFeeFromContract)
+                    EntranceFeeETH = ethers.utils.formatUnits(entranceFeeFromContract,"ether")
+                    console.log("Entrance fee is ",entranceFeeFromContract)
+                }
+            }
+            help()
+        }
+    },[isWeb3Enabled]);
+
+    return (
+    <div>
+        {   
+            contractAddress ?
+                <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee,"ether")} ETH <button onClick={ async function() {
+                        enterRaffle()
+                    }}>Enter Raffle</button>
+                </div> 
+                : 
+                <div>
+                    This network is not supported
+                </div>
+        }
+    </div>
     )
 }
---
+++ lesson10/constants/index.js
@@ -0,0 +1,7 @@
+const frontend = require("./front-end.json");
+const abi = require("./abi.json");
+
+module.exports = {
+    frontend,
+    abi,
+};
